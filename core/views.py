033c2a5ff29a91be47361f549a8beaf400b41d72
gustaoliv <gusta.oliv4@gmail.com>
2022-01-17 17:58:21 -0300

    configuração recepção de dados na views

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .forms import ContatoForm
-
+from django.contrib import messages
 #página inicial
 def index(request):
     return render(request, 'index.html')
@@ -8,11 +8,26 @@ def index(request):
 
 #página de contato
 def contato(request):
-    form = ContatoForm()
+    form = ContatoForm(request.POST or None)
+
+    if str(request.method) == 'POST':
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            mensagem = form.cleaned_data['mensagem']
+
+            print("Mensagem enviada")
+
+            messages.sucess(request, 'E-mail enviado com sucesso!') 
+            form = ContatoForm()
+        else:
+            messages.error(request, 'Erro ao enviar o e-mail')   
     context = {
         'form': form
     }
     return render(request, 'contato.html', context)
+
 
 
 #página de cadastro de produtos
