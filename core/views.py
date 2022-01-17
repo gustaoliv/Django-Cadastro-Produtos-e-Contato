eac4f60c427bf51d79420aba99172a13de503807
gustaoliv <gusta.oliv4@gmail.com>
2022-01-17 16:48:37 -0300

    criação das views, criação dos templates iniciais e criação da pasta static

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+#página inicial
+def index(request):
+    return render(request, 'index.html')
+
+
+#página de contato
+def contato(request):
+    return render(request, 'contato.html')
+
+
+#página de cadastro de produtos
+def produto(request):
+    return render(request, 'produto.html')
