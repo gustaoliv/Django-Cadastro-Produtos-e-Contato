3842ac6f58647b6f0bdbd48b1adc841efbe66a62
gustaoliv <gusta.oliv4@gmail.com>
2022-01-17 21:23:35 -0300

    configuração da tabela no index

--- core/views.py
+++ core/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .forms import ContatoForm, ProdutoModelForm
 from django.contrib import messages
+from .models import Produto
+
 #página inicial
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'produtos': Produto.objects.all()
+    }
+    return render(request, 'index.html', context)
 
 
 #página de contato
@@ -29,10 +34,16 @@ def produto(request):
     if str(request.method) == 'POST':	
         form = ProdutoModelForm(request.POST, request.FILES)
         if form.is_valid():
-            prod = form.save(commit=False)
-
+            form.save()
             messages.success(request, 'Produto salvo com sucesso!')
+            form = ProdutoModelForm()
         else:
             messages.error(request, 'Erro ao salvar o produto.')
+    else:
+        form = ProdutoModelForm()
 
-    return render(request, 'produto.html')
+    context = {
+        'form': form
+    }
+
+    return render(request, 'produto.html', context)
